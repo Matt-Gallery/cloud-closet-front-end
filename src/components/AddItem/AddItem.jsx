54f82d851a984edab5122a93bea5e67bb88ad7cb
Bri Pintens <btpintens@gmail.com>
2025-04-22 12:07:23 -0400

    finished addItem form with subcategories

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+
+const categoryOptions = {
+    Shirt: ["T-Shirt", "Tank Top", "Long Sleeve", "Turtleneck", "Button-up", "Polo", "Blouse"],
+    Pants: ["Jeans", "Khakis", "Trousers", "Leggings", "Shorts"],
+    Sweater: ["Cardigan", "Lightweight", "Turtleneck"],
+    Skirt: ["Mini", "Midi", "Maxi"],
+    Dress: ["Mini", "Midi", "Maxi", "Sleeveless"],
+    Shoes: ["Tennis Shoes", "Loafer", "Sandal", "Boot"],
+    Jacket: ["Winter", "Rain", "Blazer"]
+};
+
+const colorOptions = ["Black", "White", "Blue", "Red", "Green", "Yellow", "Gray", "Pink", "Purple", "Brown", "Pattern"];
 
 const AddItem = () => { 
-    const NewItem = ["shirt", "pants", "jacket", "shoes"];
+    const newItem = ["Shirt", "Pants", "Sweater", "Skirt", "Dress", "Shoes", "Blazer"];
 
-    const [formData, setFormData] =useState({
-        name: "",
-        type: clothingOptions[0],
+    const [formData, setFormData] = useState({
+        title: "",
+        category: "Shirt",
+        subcategory: categoryOptions["Shirt"][0],
+        color: "",
     });
    
     const handleChange = (e) => {
         const { name, value } = e.target; 
-        setFormData((prev) => ({
+            if (name === "category"){
+              setFormData((prev) => ({
             ...prev,
+            category: value, 
+            subcategory: categoryOptions[value][0],
+        }));
+    } else {
+        setFormData((prev) => ({
+            ...prev, 
             [name]: value, 
         }));
+    }
     };
 const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,37 +48,70 @@ return (
     <h1>Add New Item to Closet</h1>
     <form onSubmit={handleSubmit}>
         <label>
-            Item Name: 
+            Custom Title: 
             <input
             type="text"
-            name="name"
+            name="title"
             value={formData.name}
             onChange={handleChange}
+            placeholder="e.g., Yellow Button-Up from Gap"
             required
             />
         </label>
         <br />
+
         <label>
-            Clothing Type: 
+            Category: 
             <select
-            name="type"
-            value={formData.type}
+            name="category"
+            value={formData.category}
             onChange={handleChange}
+            required
             >
-                {clothingOptions.map((option, idx) => (
-                    <option key={idx} value={option}>
-                        {option}
-                    </option>
+                 {Object.keys(categoryOptions).map((category, idx) => (
+        <option key={idx} value={category}>
+          {category}
+        </option>
                 ))}
             </select>
         </label>
         <br />
+
+        <label>
+            Subcategory: 
+            <select
+            name="subcategory"
+            value={formData.subcategory}
+            onChange={handleChange}
+            >
+             {categoryOptions[formData.category].map((sub, idx) => (
+        <option key={idx} value={sub}>
+          {sub}
+        </option>
+      ))} 
+            </select>
+        </label>
+        <br />
+
+        <label>
+            Color (optional):
+            <select
+            name="color"
+            value={formData.color}
+            onChanged={handleChange}
+            >
+            <option value="">--Select a Color--</option>
+            {colorOptions.map((color, idx) => (
+                <option key={idx} value={color}>
+                    {color}
+                </option>
+            ))}
+            </select>
+        </label>
+        <br />
+
         <button type="submit">Add Item</button>
         </form>
-
-        <p>
-            Preview: {formData.name ? `${formData.name} (${formData.type})` : "No item yet"}
-        </p>
     </>
 );
 };
